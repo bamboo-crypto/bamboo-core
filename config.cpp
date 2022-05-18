7c3db68c21e8c9edd373ba3ab158af5a64935b2d
MrPandaBear <mrpandabear@mrpandabear.org>
2022-05-18 04:23:16 -0700

    add pufferfish

--- config.cpp
+++ config.cpp
@@ -116,6 +116,7 @@ json getConfig(int argc, char**argv) {
     } else if (testnet) {
         config["hostSources"].push_back("http://54.185.169.234:3000/peers");
     } else {
+        config["hostSources"].push_back("https://mrpandabear.org/node/peers");
         config["hostSources"].push_back("http://ec2-35-84-249-159.us-west-2.compute.amazonaws.com:3000/peers");
         config["hostSources"].push_back("http://ec2-44-227-179-62.us-west-2.compute.amazonaws.com:3000/peers");
         config["hostSources"].push_back("http://ec2-54-189-82-240.us-west-2.compute.amazonaws.com:3000/peers");
